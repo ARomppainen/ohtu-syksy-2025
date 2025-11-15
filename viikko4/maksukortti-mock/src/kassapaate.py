b51d1d6ed110d7f65a5dbccb1e7df0677860622c
Aleksi Romppainen <aromppa@gmail.com>
2025-11-15 20:00:35 +0200

    Add test cases using Mock-objects and fix code
    Week 4, task 2

--- viikko4/maksukortti-mock/src/kassapaate.py
+++ viikko4/maksukortti-mock/src/kassapaate.py
@@ -1,3 +1,6 @@
+from maksukortti import Maksukortti
+
+
 HINTA = 5
 
 
@@ -5,9 +8,13 @@ class Kassapaate:
     def __init__(self):
         self.__myytyja_lounaita = 0
 
-    def lataa(self, kortti, summa):
+    def lataa(self, kortti: Maksukortti, summa):
+        if summa <= 0:
+            return
         kortti.lataa(summa)
 
-    def osta_lounas(self, kortti):
+    def osta_lounas(self, kortti: Maksukortti):
+        if kortti.saldo() < HINTA:
+            return
         kortti.osta(HINTA)
         self.__myytyja_lounaita = self.__myytyja_lounaita + 1
--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -11,7 +11,7 @@ class TestKassapaate(unittest.TestCase):
     def test_kortilta_velotetaan_hinta_jos_rahaa_on(self):
         maksukortti_mock = Mock()
         maksukortti_mock.saldo.return_value = 10
-        
+
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_called_with(HINTA)
@@ -19,7 +19,22 @@ class TestKassapaate(unittest.TestCase):
     def test_kortilta_ei_veloteta_jos_raha_ei_riita(self):
         maksukortti_mock = Mock()
         maksukortti_mock.saldo.return_value = 4
-        
+
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_lataus_lisaa_kortin_saldoa_jos_summa_positiivinen(self):
+        maksukortti_mock = Mock(wraps=Maksukortti(5))
+        self.kassa.lataa(maksukortti_mock, 10)
+        maksukortti_mock.lataa.assert_called_with(10)
+
+    def test_lataus_ei_tee_mitaan_jos_summa_negatiivinen(self):
+        maksukortti_mock = Mock(wraps=Maksukortti(5))
+        self.kassa.lataa(maksukortti_mock, -1)
+        maksukortti_mock.lataa.assert_not_called()
+
+    def test_lataus_ei_tee_mitaan_jos_summa_nolla(self):
+        maksukortti_mock = Mock(wraps=Maksukortti(5))
+        self.kassa.lataa(maksukortti_mock, 0)
+        maksukortti_mock.lataa.assert_not_called()
