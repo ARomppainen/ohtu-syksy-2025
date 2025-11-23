92d67b3817f42471cd170366160617a912638edc
Aleksi Romppainen <aromppa@gmail.com>
2025-11-23 19:20:54 +0200

    Extract scoring of advantage or won game

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -13,39 +13,30 @@ class TennisGame:
             self.m_score2 = self.m_score2 + 1
 
     def get_score(self):
-        score = ""
-        temp_score = 0
-
         if self._is_even_game():
             return self._score_even_game()
 
-        if self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self.m_score2
+        if self._is_advantage_or_won_game():
+            return self._score_advantage_or_won_game()
 
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
+        score = ""
+        temp_score = 0
+
+        for i in range(1, 3):
+            if i == 1:
+                temp_score = self.m_score1
             else:
-                score = "Win for player2"
-        else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
+                score = score + "-"
+                temp_score = self.m_score2
 
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+            if temp_score == 0:
+                score = score + "Love"
+            elif temp_score == 1:
+                score = score + "Fifteen"
+            elif temp_score == 2:
+                score = score + "Thirty"
+            elif temp_score == 3:
+                score = score + "Forty"
 
         return score
 
@@ -60,3 +51,17 @@ class TennisGame:
         if self.m_score2 == 2:
             return "Thirty-All"
         return "Deuce"
+
+    def _is_advantage_or_won_game(self):
+        return self.m_score1 >= 4 or self.m_score2 >= 4
+
+    def _score_advantage_or_won_game(self):
+        difference = self.m_score1 - self.m_score2
+
+        if difference == 1:
+            return "Advantage player1"
+        if difference == -1:
+            return "Advantage player2"
+        if difference >= 2:
+            return "Win for player1"
+        return "Win for player2"
