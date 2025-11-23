71cec22b02b0e7c4959127e9856aa66da16e4b0e
Aleksi Romppainen <aromppa@gmail.com>
2025-11-23 21:23:07 +0200

    Apply suggestions

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -3,6 +3,7 @@ from typing import Literal
 _WIN_THRESHOLD = 4
 _DEUCE_THRESHOLD = 3
 
+Player = Literal["player1", "player2"]
 PlayerNumber = Literal[1, 2]
 
 
@@ -14,15 +15,13 @@ class TennisGame:
         self.player1_points = 0
         self.player2_points = 0
 
-    def won_point(self, player_name: str):
-        if player_name == self.player1_name:
+    def won_point(self, player: Player) -> None:
+        if player == "player1":
             self.player1_points += 1
-        elif player_name == self.player2_name:
-            self.player2_points += 1
         else:
-            raise ValueError(f'Unknown player: "{player_name}"')
+            self.player2_points += 1
 
-    def get_score(self):
+    def get_score(self) -> str:
         if self._is_even_game():
             return self._score_even_game()
 
@@ -34,18 +33,18 @@ class TennisGame:
     def _is_even_game(self) -> bool:
         return self.player1_points == self.player2_points
 
-    def _score_even_game(self):
+    def _score_even_game(self) -> str:
         if self.player1_points >= _DEUCE_THRESHOLD:
             return "Deuce"
         return f"{self._points_to_score(self.player1_points)}-All"
 
-    def _is_advantage_or_won_game(self):
+    def _is_advantage_or_won_game(self) -> bool:
         return (
             self.player1_points >= _WIN_THRESHOLD
             or self.player2_points >= _WIN_THRESHOLD
         )
 
-    def _score_advantage_or_won_game(self):
+    def _score_advantage_or_won_game(self) -> str:
         points_difference = self.player1_points - self.player2_points
 
         if points_difference == 1:
@@ -57,20 +56,20 @@ class TennisGame:
         return self._score_win(2)
 
     @staticmethod
-    def _score_advantage(player: PlayerNumber):
+    def _score_advantage(player: PlayerNumber) -> str:
         return f"Advantage player{player}"
 
     @staticmethod
-    def _score_win(player: PlayerNumber):
+    def _score_win(player: PlayerNumber) -> str:
         return f"Win for player{player}"
 
-    def _score_other_cases(self):
+    def _score_other_cases(self) -> str:
         score1 = self._points_to_score(self.player1_points)
         score2 = self._points_to_score(self.player2_points)
         return f"{score1}-{score2}"
 
     @staticmethod
-    def _points_to_score(points: int):
+    def _points_to_score(points: int) -> str:
         if points == 0:
             return "Love"
         if points == 1:
